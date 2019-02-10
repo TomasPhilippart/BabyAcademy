3cb2da53af32ad617bb03a7d43f94733143fd92f
Tomás Philippart <tomasphilippart@gmail.com>
2019-02-10 17:07:48 +0000

    Updating course

--- scripts/word_course.js
+++ scripts/word_course.js
@@ -6,7 +6,14 @@ var myArray = [
 
 var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
 
+function getStarted() {
+   document.getElementById('word').innerHTML = randomItem;
+   document.getElementById('word').style.display = "block";
+   document.getElementById('hidden').style.display = "none";
+}
 
-window.onload = function(){
-    document.getElementById("word").innerHTML = randomItem;
+window.onclick = showWords();
+
+function showWords() {
+  document.getElementById("word").innerHTML = randomItem;
 }
