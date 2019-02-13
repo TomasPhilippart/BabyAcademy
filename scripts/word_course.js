8c794a137ab35bdef968230f1ae2563b85ac8a9c
TomasPhilippart <tomasphilippart@gmail.com>
2019-02-13 15:55:10 +0000

    Trying some stuff

--- scripts/word_course.js
+++ scripts/word_course.js
@@ -8,11 +8,7 @@ var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
 
 function getStarted() {
    document.getElementById('word').innerHTML = randomItem;
+   document.getElementById('word_course').style.height = "100%";
    document.getElementById('word').style.display = "block";
    document.getElementById('hidden').style.display = "none";
-}
-
-function showNewWord() {
-  var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
-  document.getElementById('word').innerHTML = randomItem;
 }
